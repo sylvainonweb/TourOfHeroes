a2891155a801d15bfd7cdd71a5c4d1b31593d82f
sylvainonweb <sylvain.onweb@gmail.com>
2018-06-08 14:41:00 +0200

    Liste des héros + affichage du héro sélectionné

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero, HEROES } from './hero';
+
+@Component({
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+    heroes: Hero[];
+    selectedHero: Hero;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+        this.heroes = [];
+        for (let i = 1; i < 10; i++) {
+            const hero: Hero = new Hero();
+            hero.id = i;
+            hero.name = 'Héro ' + i;
+            this.heroes.push(hero);
+        }
+    }
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+}

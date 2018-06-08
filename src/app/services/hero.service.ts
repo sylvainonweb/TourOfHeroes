fe34db76ecce63b5833bc45efa37fb995ff2237b
sylvainonweb <sylvain.onweb@gmail.com>
2018-06-08 15:59:26 +0200

    Affichage d'une liste de messages

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Hero, HEROES } from '../hero';
 import { Observable, of} from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class HeroService {
 
-    constructor() { }
+    constructor(private messageService: MessageService) { }
 
     getHeroes(): Observable<Hero[]> {
+
+        this.messageService.add('heroService: Chargement des héroes');
+
         const heroes: Hero[] = [];
         for (let i = 1; i < 10; i++) {
             const hero: Hero  = new Hero();
@@ -17,6 +21,7 @@ export class HeroService {
             hero.name = 'Héro ' + i;
             heroes.push(hero);
         }
+
         return of(heroes);
     }
 }
